a6c738d6044315a455168330314d5fc7ba53c232
tbturk <tbturkk@gmail.com>
2019-10-09 21:10:08 +0300

    1. ister liste dolaşma fonksiyonu eklendi

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
 #include <math.h>
+#include <windows.h>
 //batuhanin yazdigi kisim
 //ibr
 //alo
@@ -116,12 +117,60 @@ void islemSecim();
 void dosyaKontrol();
 void listdir();
 
+int ListDirectoryContents(const char *sDir)
+{
+    WIN32_FIND_DATA fdFile;
+    HANDLE hFind = NULL;
+
+    char sPath[2048];
+
+    //Specify a file mask. *.* = We want everything!
+    sprintf(sPath, "%s\\*.*", sDir);
+
+    if((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+    {
+        printf("Path not found: [%s]\n", sDir);
+        return 0;
+    }
+
+    do
+    {
+        //Find first file will always return "."
+        //    and ".." as the first two directories.
+        if(strcmp(fdFile.cFileName, ".") != 0
+                && strcmp(fdFile.cFileName, "..") != 0)
+        {
+            //Build up our file path using the passed in
+            //  [sDir] and the file/foldername we just found:
+            sprintf(sPath, "%s\\%s", sDir, fdFile.cFileName);
+
+            //Is the entity a File or Folder?
+            if(fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
+            {
+                printf("Directory: %s\n", sPath);
+                ListDirectoryContents(sPath); //Recursion, I love it!
+            }
+            else{
+                printf("File: %s\n", sPath);
+            }
+        }
+    }
+    while(FindNextFile(hFind, &fdFile)); //Find the next file.
+
+    FindClose(hFind); //Always, Always, clean things up!
+
+    return 1;
+}
+
+
 
 int main(int argc, char **argv)
 {
+    ListDirectoryContents("./");
+
 	karsilamaEkrani();
 	islemSecim();
-
+    //listdir(".",0);
 
 
 
